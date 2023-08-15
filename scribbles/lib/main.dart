06f753b5e85e50292749bae36e53da793e8c5a1d
Maak 47 <oshaf.khan10@gmail.com>
2023-08-15 16:21:42 +0530

    Added Firebase and Splash

--- scribbles/lib/main.dart
+++ scribbles/lib/main.dart
@@ -1,8 +1,11 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
 
 import 'package:scribbles/screens/auth.dart';
+import 'firebase_options.dart';
+import 'package:scribbles/screens/chat.dart';
+import 'package:scribbles/screens/splash.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -24,6 +27,17 @@ class App extends StatelessWidget {
           colorScheme: ColorScheme.fromSeed(
               seedColor: const Color.fromARGB(255, 63, 17, 177)),
         ),
-        home: const AuthScreen());
+        home: StreamBuilder(
+          stream: FirebaseAuth.instance.authStateChanges(),
+          builder: (ctx, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const SplashScreen();
+            }
+            if (snapshot.hasData) {
+              return const ChatScreen();
+            }
+            return const AuthScreen();
+          },
+        ));
   }
 }
