9f35866e0418e71addcfb000ff52a06ce141d564
Maak 47 <maak.47000@gmail.com>
2023-10-02 10:20:30 +0530

    deleted projects

--- inner_voice/android/app/src/main/kotlin/com/example/inner_voice/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.inner_voice
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- inner_voice/lib/utilities/colors.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-const kBackgroundColor = Color.fromRGBO(0, 0, 0, 1);
-const kPrimaryColor = Colors.amber;
-const kSecondaryColor = Colors.white;
-const kTertiaryColor = Colors.grey;
