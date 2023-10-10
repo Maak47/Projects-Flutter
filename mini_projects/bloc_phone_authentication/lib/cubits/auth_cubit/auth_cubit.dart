9f33d44813cdeba2e9510f23407bc7af7c33a2e5
Maak 47 <maak.47000@gmail.com>
2023-10-11 00:00:59 +0530

    functions

--- mini_projects/bloc_phone_authentication/lib/cubits/auth_cubit/auth_cubit.dart
+++ mini_projects/bloc_phone_authentication/lib/cubits/auth_cubit/auth_cubit.dart
@@ -5,7 +5,16 @@ import './auth_state.dart';
 class AuthCubit extends Cubit<AuthState> {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   AuthCubit() : super(AuthInitial()) {
-    void sendOTP(String phoneNumber) async {}
+    void sendOTP(String phoneNumber) async {
+      await _auth.verifyPhoneNumber(
+        phoneNumber: phoneNumber,
+        codeSent: (verificationId, forceResendingToken) {},
+        verificationCompleted: (phoneAuthCredential) {},
+        verificationFailed: (error) {},
+        codeAutoRetrievalTimeout: (verificationId) {},
+      );
+    }
+
     void verifyOTP(String otp) async {}
 
     void signInWithPhone() async {}
