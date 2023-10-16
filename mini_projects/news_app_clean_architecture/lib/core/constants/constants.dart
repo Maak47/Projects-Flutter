d1d6dbd24a888137367a6693c8ce9ad1a1c665ca
Maak 47 <maak.47000@gmail.com>
2023-10-16 16:06:13 +0530

    resources+domain+data

---
+++ mini_projects/news_app_clean_architecture/lib/core/constants/constants.dart
@@ -0,0 +1,5 @@
+const String newsApiBaseURL = 'https://newsapi.org/v2';
+
+const String newsApiKey = 'd529c938a2d04ce4856800f8678e4177';
+const String countryQuery = 'in';
+const String categoryQuery = 'general';
---
+++ mini_projects/news_app_clean_architecture/lib/features/daily_news/presentation/bloc/article/remote/remote_article_state.dart
@@ -0,0 +1,28 @@
+import 'package:dio/dio.dart';
+import 'package:equatable/equatable.dart';
+import 'package:news_app_clean_architecture/features/daily_news/domain/entities/article.dart';
+
+abstract class RemoteArticleState extends Equatable {
+  final List<ArticleEntity>? articles;
+  final DioException? exception;
+
+  const RemoteArticleState({this.articles, this.exception});
+
+  @override
+  List<Object?> get props => [
+        articles!,
+        exception!,
+      ];
+}
+
+class RemoteArticlesLoading extends RemoteArticleState {}
+
+class RemoteArticlesDone extends RemoteArticleState {
+  const RemoteArticlesDone(List<ArticleEntity> article)
+      : super(articles: article);
+}
+
+class RemoteArticlesError extends RemoteArticleState {
+  const RemoteArticlesError(DioException exception)
+      : super(exception: exception);
+}
