365aeaa7cd972ca7b79e069ad5dc6a4ff20b35df
maak47 <oshaf.khan10@gmail.com>
2023-11-10 20:46:09 +0530

    init

---
+++ mini_projects/shapes/lib/screens/profile.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class ProfileScreen extends StatelessWidget {
+  const ProfileScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My Profile'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                backgroundColor: Colors.grey,
+                radius: 75,
+              ),
+
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
