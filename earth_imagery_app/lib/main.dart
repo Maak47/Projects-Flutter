48386f52a859b7067b66a124c55aafe2d939865c
maak47 <oshaf.khan10@gmail.com>
2023-11-25 22:25:11 +0530

    sda

--- earth_imagery_app/lib/main.dart
+++ earth_imagery_app/lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:earth_imagery_app/screens/auth.dart';
 import 'package:earth_imagery_app/widgets/bottomnav.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
