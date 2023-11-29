6d2fbab89b8c9aa2e68939c0d717999ebb231f4c
Maak47 <oshaf.khan10@gmail.com>
2023-11-29 17:31:07 +0530

    del

--- mini_projects/travel_app_clean_architecture/android/app/src/main/kotlin/com/example/travel_app_clean_architecture/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.travel_app_clean_architecture
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- mini_projects/travel_app_clean_architecture/lib/features/trip/domain/repositories/trip_repository.dart
+++
@@ -1,7 +0,0 @@
-import 'package:travel_app_clean_architecture/features/trip/domain/entities/trip_entity.dart';
-
-abstract class TripRepository {
-  Future<List<TripEntity>> getTrips();
-  Future<void> addTrips(TripEntity tripEntity);
-  Future<void> deleteTrips(int index);
-}
