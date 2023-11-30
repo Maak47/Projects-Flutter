c3671dd5f9bacbdf8a4b96082dabf82150db353a
Maak47 <oshaf.khan10@gmail.com>
2023-11-30 18:49:54 +0530

    init

---
+++ mini_projects/movie_app_tdd_bloc/lib/features/domain/entities/movie_entity.dart
@@ -0,0 +1,13 @@
+class Movie {
+  final String title;
+  final String id;
+
+  final String overview;
+  final String posterPath;
+
+  Movie(
+      {required this.title,
+      required this.id,
+      required this.overview,
+      required this.posterPath});
+}
