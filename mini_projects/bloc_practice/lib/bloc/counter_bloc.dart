38c94906dc8906438ca2271a4485b325ea9f0aaf
Maak47 <oshaf.khan10@gmail.com>
2023-12-08 23:28:19 +0530

    simple increment

---
+++ mini_projects/bloc_practice/lib/bloc/counter_bloc.dart
@@ -0,0 +1,10 @@
+import 'package:bloc_practice/bloc/counter_event.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class CounterBloc extends Bloc<CounterEvent, int> {
+  CounterBloc() : super(0) {
+    on<CounterIncrementPressed>((event, emit) {
+      emit(state + 1);
+    });
+  }
+}
--- mini_projects/bloc_practice/lib/main.dart
+++ mini_projects/bloc_practice/lib/main.dart
@@ -1,1 +1,50 @@
+// main.dart
 
+import 'package:bloc_practice/bloc/counter_bloc.dart';
+import 'package:bloc_practice/bloc/counter_event.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+void main() async {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(useMaterial3: true),
+      home: BlocProvider(
+        create: (context) => CounterBloc(),
+        child: MyHomePage(), // Use MyHomePage as the child widget
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Bloc Counter'),
+      ),
+      body: Center(
+        child: BlocBuilder<CounterBloc, int>(
+          builder: (context, state) {
+            return Text('$state', style: Theme.of(context).textTheme.headline4);
+          },
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        key: const Key('counterView_increment_floatingActionButton'),
+        child: const Icon(Icons.add),
+        onPressed: () {
+          BlocProvider.of<CounterBloc>(context).add(CounterIncrementPressed());
+        },
+      ),
+    );
+  }
+}
